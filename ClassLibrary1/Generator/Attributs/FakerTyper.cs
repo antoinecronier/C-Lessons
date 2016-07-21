fec49a7618e1dd81f6ede593d8c06b440dabe273
Antoine Cronier <antoine.cronier@tactfactory.com>
2016-07-21 13:38:19 +0200

    SQLite extension reflexion

---
+++ ClassLibrary1/Generator/Attributs/FakerTyper.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibrary2.Entities.Generator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2.Attributs
+{
+    [System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Property)]
+    public class FakerTyper : System.Attribute
+    {
+        private string name;
+
+        public TypeEnumCustom Typer { get; set; }
+
+        public FakerTyper(string name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ ClassLibrary1/Generator/TypeEnumCustom.cs
@@ -0,0 +1,15 @@
+﻿using ClassLibrary2.EnumManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2.Entities.Generator
+{
+    public class TypeEnumCustom
+    {
+        public const String NAME = "name";
+        public const String SURNAME = "surname";
+    }
+}
